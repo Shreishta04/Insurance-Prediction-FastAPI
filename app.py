38b548982381cca89c2e2ab355efed60f2ccdb04
Shreishta Manoj <shreishtamanoj04@gmail.com>
2026-01-08 11:23:54 +0530

    Edited CORS

--- app.py
+++ app.py
@@ -24,10 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://192.168.29.35:5173",   # ✅ ACTUAL frontend
-        "http://192.168.29.35:3000",   # optional
-        "http://localhost:5173",
-        "http://localhost:3000"
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
